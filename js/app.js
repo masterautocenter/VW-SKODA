626b5b15468808292824e0e10c7ddc064b8f5b0d
aminbenjilali <amin.benjilali@student.odisee.be>
2018-09-06 21:00:54 +0200

    Language Pack

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzT6UG1sbkMMVo0MAn5WWYps_tnEI2JoRB_XjJIFmEB9KkmRWQz/exec'  
-                  const form = document.forms['enq']  
-                  form.addEventListener('submit', e => {  
-                   e.preventDefault()  
-                   fetch(scriptURL, { method: 'POST', body: new FormData(form)})  
-                    .then(response => window.location.href = 'endEn.html')  
-                    .catch(error => console.error('Error!', error.message))
-                      console.log("test");
-                  })  
-
-
-
-$(document).ready(function(){
+$(document).ready(function () {
 //  Check Radio-box
     $(".q1 input:radio").attr("checked", false);
     $('.q1 input').click(function () {
@@ -19,7 +7,7 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 //  Check Radio-box
     $(".q2 input:radio").attr("checked", false);
     $('.q2 input').click(function () {
@@ -28,7 +16,7 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
 //  Check Radio-box
     $(".q3 input:radio").attr("checked", false);
     $('.q3 input').click(function () {
@@ -36,8 +24,8 @@ $(document).ready(function(){
         $(this).parent().addClass('checked');
     });
 });
-function half(){
-   if (document.forms["enq"]["nummerplaat"].value.length > 0 && $('input[type=radio][name=raadgever]:checked').size() > 0 && $('input[type=radio][name=enquete]:checked').size() > 0 && $('input[type=radio][name=merk]:checked').size() > 0){
+function half() {
+    if (document.forms["enq"]["nummerplaat"].value.length > 0 && $('input[type=radio][name=raadgever]:checked').size() > 0 && $('input[type=radio][name=enquete]:checked').size() > 0 && $('input[type=radio][name=merk]:checked').size() > 0){
        document.getElementById("firstHalf").style.display="none"
     document.getElementById("secondHalf").style.display="inherit"}
 }
